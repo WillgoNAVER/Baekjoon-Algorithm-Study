386f6661e4fd13370b7798e70d9bdbbae28265e6
Kim Ki hyeon <141827027+WillgoNAVER@users.noreply.github.com>
2024-10-02 11:17:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 452 ms, Memory: 50756 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -45,27 +45,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        HashMap<Integer, String> bookName = new HashMap<>();
-        HashMap<String, Integer> bookNo = new HashMap<>();
+        HashMap<String, Integer> bookName = new HashMap<>();
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(reader.readLine());
         int N = Integer.parseInt(st.nextToken());
+        String pocketMonArr[] = new String[N + 1];
         int M = Integer.parseInt(st.nextToken());
 
         for (int i = 1; i <= N; i++) {
             String input = reader.readLine();
-            bookName.put(i, input);
-            bookNo.put(input, i);
+            bookName.put(input, i);
+            pocketMonArr[i] = input;
         }
 
         for (int i = 0; i < M; i++) {
-            String search = reader.readLine();
+            String search = reader.readLine().trim();
             char c = search.charAt(0);
             if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) { // 이름이 입력으로 들어오면 숫자를 출력해야함.
-                sb.append(bookNo.get(search)).append('\n');
+                sb.append(bookName.get(search)).append('\n');
             } else { // 숫자가 입력으로 들어오면 이름을 입력해야함.
-                int No = Integer.parseInt(search);
-                sb.append(bookName.get(No)).append('\n');
+                sb.append(pocketMonArr[Integer.parseInt(search)]).append('\n');
             }
         }
         System.out.println(sb);
