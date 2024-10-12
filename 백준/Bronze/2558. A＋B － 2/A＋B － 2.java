4b7fc3bad63524d25eda70406aefa028e1db49fa
Kim Ki hyeon <141827027+WillgoNAVER@users.noreply.github.com>
2024-10-12 20:19:46 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A+B);
+    }
+}
