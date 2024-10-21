0d51556a05a76f8fcb333e269d9f06bca1cebdef
Kim Ki hyeon <141827027+WillgoNAVER@users.noreply.github.com>
2024-10-21 18:50:15 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2220 ms, Memory: 159868 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Integer arr[] = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer i : arr) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
