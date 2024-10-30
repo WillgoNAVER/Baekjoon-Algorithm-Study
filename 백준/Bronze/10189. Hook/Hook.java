6d5c2e30a14c762517f3223ea388efccd8d7e7b3
Kim Ki hyeon <141827027+WillgoNAVER@users.noreply.github.com>
2024-10-30 23:26:23 +0900

    [Bronze V] Title: Hook, Time: 88 ms, Memory: 13760 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("#  # #### #### #  #").append('\n');
+        sb.append("#### #  # #  # # #").append('\n');
+        sb.append("#### #  # #  # # #").append('\n');
+        sb.append("#  # #### #### #  #").append('\n');
+
+        System.out.println(sb);
+    }
+}
